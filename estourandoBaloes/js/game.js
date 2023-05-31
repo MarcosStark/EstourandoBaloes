cb5cc5fcec005cfb6e361aa96f84f8a667438c29
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-05-30 23:27:40 -0300

    function gameTime

--- estourandoBaloes/js/game.js
+++ estourandoBaloes/js/game.js
@@ -1,5 +1,25 @@
 function start(){
     let dificultity = document.getElementById("dificultity").value;
-    console.log(dificultity);
+    time(dificultity)
+}
+
+function time(value){
+
+    let gameTime;
+
+    if(value == 1){
+
+        gameTime = 90;
+
+    } else if(value == 2){
+
+        gameTime = 60;
+
+    } else {
+
+        gameTime = 30;
+    }
+
+    console.log(gameTime);
 }
 
