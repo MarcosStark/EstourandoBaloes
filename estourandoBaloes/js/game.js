c1c49f1c62f5133592f205d42cd6848ca45e1996
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-06-01 23:38:48 -0300

    update game.js

--- estourandoBaloes/js/game.js
+++ estourandoBaloes/js/game.js
@@ -1,3 +1,4 @@
+var qtd_baloes = 80;
 function start(){
     let dificultity = document.querySelector("#dificultity").value;
     time(dificultity);
@@ -21,12 +22,26 @@ function time(value){
     }
 
     localStorage.setItem("gameTime", gameTime);
-    
+
+ 
 }
 
-(function (){
+(function(){
 
-   document.querySelector("#timer").innerHTML =  localStorage.getItem("gameTime");
-   
+    document.querySelector("#timer").innerHTML =  localStorage.getItem("gameTime");
+
+    document.querySelector("#whole-balloons").innerHTML = qtd_baloes;
+    document.querySelector("#popped-balloons").innerHTML = 0;
+    createBalloons(qtd_baloes);
+
 })();
 
+function createBalloons(qtd_baloes) {
+
+    for(var i = 1; i <= qtd_baloes; i++){
+    var balloon = document.createElement("img"); // Cria o elemento img na div do id "cenario"
+    balloon.src = "css/images/balao_azul_pequeno.png";
+    document.getElementById("game").appendChild(balloon);
+    
+    }
+}
