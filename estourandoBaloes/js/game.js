c48f751702198d09523f62df9eda34c3292050b8
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-06-07 20:48:56 -0300

    Update game.js

--- estourandoBaloes/js/game.js
+++ estourandoBaloes/js/game.js
@@ -1,4 +1,6 @@
-var qtd_baloes = 72;
+var qtd_balloons = 72;
+var timer_id = null;
+
 function start(){
     let dificultity = document.querySelector("#dificultity").value;
     time(dificultity);
@@ -24,7 +26,6 @@ function time(value){
     }
 
     localStorage.setItem("gameTime", gameTime);
-
  
 }
 
@@ -32,30 +33,57 @@ function time(value){
 
     document.querySelector("#timer").innerHTML =  localStorage.getItem("gameTime");
 
-    document.querySelector("#whole-balloons").innerHTML = qtd_baloes;
+    document.querySelector("#whole-balloons").innerHTML = qtd_balloons;
     document.querySelector("#popped-balloons").innerHTML = 0;
-    createBalloons(qtd_baloes);
+    createBalloons(qtd_balloons);
+    timeCount(localStorage.getItem("gameTime"));
 
 })();
 
-function createBalloons(qtd_baloes) {
+function createBalloons(qtd_balloons) {
 
-    for(var i = 1; i <= qtd_baloes; i++){
+    for(var i = 1; i <= qtd_balloons; i++){
     var balloon = document.createElement("img"); // Cria o elemento img na div do id "cenario"
     balloon.src = "css/images/small_blue_balloon.png";
     balloon.style.margin = "8px";
     balloon.id = 'b' + i;
-    balloon.onclick = function(){estourar_baloes(this);};
+    balloon.onclick = function(){popBalloons(this);};
     document.querySelector("#balloons").appendChild(balloon);
     
     }
 }
 
-function estourar_baloes(e){
+function popBalloons(e){
 
     var id_balao = e.id;
 
     document.getElementById(id_balao).setAttribute("onclick", "");
     document.getElementById(id_balao).src = "css/images/small_blue_balloon_burst.png";
 
+}
+
+
+function timeCount(time){
+
+    time = time - 1;
+
+    if(time == -1){
+        stopGame();
+        gameOver();
+        return false;
+    }
+
+    document.getElementById("timer").innerHTML = time;
+
+    timer_id = setTimeout("timeCount("+time+")", 1000);
+}
+
+function gameOver(){
+  
+    alert("Fim de jogo! Você não conseguiu estourar todos os balões!"); 
+
+}
+
+function stopGame(){
+    clearTimeout(timer_id);
 }
